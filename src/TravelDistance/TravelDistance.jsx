e63ee2ee2391961b8927d79acabf560bad32fe18
kosimovdev <kosimov.oyatillo@gmail.com>
2025-03-23 00:10:57 +0900

    HeartActivity page , TravelDistance page, login page react-router-dom is done

--- src/TravelDistance/TravelDistance.jsx
+++ src/TravelDistance/TravelDistance.jsx
@@ -21,12 +21,14 @@ function TravelDistance() {
             <div className="flex flex-col items-center justify-center">
                 <div className="bg-white p-6 rounded-2xl shadow-lg w-80 text-center">
                     <h1 className="text-xl font-bold">이동거리</h1>
+
                     <div className="relative flex items-center  m-auto mt-4">
                         <div>
                             <button onClick={handlePreviousClick} className="flex items-center justify-center rounded-full m-auto w-[40px] h-[40px] bg-[#E2E2E2]"><img className={"w-[20px]"} src={previous} alt="previousLogo"/></button>
                         </div>
                         <div className="w-32 h-32 bg-[#DAD7FE] rounded-full m-auto flex items-center justify-center border-[10px] border-[#4339F2]">
                             <span className="text-2xl font-bold text-[#4339F2]">{bpm}</span>
+
                         </div>
                         <div>
                             <button onClick={handleNextClick} className="flex items-center justify-center rounded-full m-auto w-[40px] h-[40px] bg-[#E2E2E2]"><img className={"w-[20px]"} src={nextLogo} alt="previousLogo"/></button>
@@ -34,6 +36,7 @@ function TravelDistance() {
                     </div>
                     <p className="mt-4 flex items-center text-black">
                         <span className="w-4 h-4 bg-[#4339F2] rounded-full inline-block mr-2"></span> KM
+
                     </p>
                 </div>
             </div>
