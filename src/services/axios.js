fe72493ee7f2b73e1606d144f12e07614e60abbf
kosimovdev <kosimov.oyatillo@gmail.com>
2025-05-13 16:39:24 +0900

    some changes

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,22 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // 🔥 .env dan olib ishlaydi
-//   baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
 export default api;
-// import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "ngrok-skip-browser-warning": "true",
-//   },
-// });
-
-// export default api;
