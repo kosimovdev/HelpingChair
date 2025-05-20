c5db422c7761a8cb60b2dc1dec137049e9a9dc52
kosimovdev <kosimov.oyatillo@gmail.com>
2025-05-20 16:24:09 +0900

    some changes

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
     outDir: "dist",
   },
   server: {
-    proxy: {
-      "/api": {
-        target: "https://3c20-14-42-86-31.ngrok-free.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://3c20-14-42-86-31.ngrok-free.app",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
     historyApiFallback: true, // 👈 bu qo‘shildi
   },
 });
