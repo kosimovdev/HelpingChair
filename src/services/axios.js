d15c2af8dc70314a2341a8e91bf497a0c5eed3c8
kosimovdev <kosimov.oyatillo@gmail.com>
2025-06-09 12:31:08 +0900

    some changes

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  // baseURL: "https://gilbeot.up.railway.app",
+  withCredentials: false,
   headers: {
     "Content-Type": "application/json",
     // "ngrok-skip-browser-warning": "true",
