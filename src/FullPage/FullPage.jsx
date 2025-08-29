1c7479471615ce5da2d36d9e8a92dc783b2f3305
kosimovdev <kosimov.oyatillo@gmail.com>
2025-08-29 20:16:58 +0900

    some style changes

--- src/FullPage/FullPage.jsx
+++ src/FullPage/FullPage.jsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { HeartPulse, Clock, MapPin, Camera } from "lucide-react"; // 🔹 lucide-react iconlar
+import storage from "../services/storage/storage";
 
 export default function FullPage() {
+    const user_id = storage.get("user_id");
+    const navigate = useNavigate();
   const menus = [
     { id: 1, title: "심박수", path: "/heart" , icon:<HeartPulse size={140} className="text-[#fff]" /> ,  bgColor:"bg-red-500"},
     { id: 2, title: "활동 시간", path: "/activity" ,  icon: <Clock size={140} className="text-white" /> , bgColor:"bg-[#0077b6]"}, 
     { id: 3, title: "지도", path: "/map" , icon: <MapPin size={140} className="text-white" /> , bgColor:"bg-[#f2c203ff]"},
     { id: 4, title: "Camera", path: "/camera" , icon: <Camera size={140} className="text-white" /> , bgColor:"bg-gray-500"},
   ];
+
+  useEffect(() => {
+    console.log("User ID:", user_id);
+    if(!user_id) {
+      console.error("User ID is not available.");
+      return navigate("/login");
+    }
+  }, [user_id]);
 
   return (
     <div className="flex items-center justify-center  p-2">
